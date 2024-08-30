d39c3d9410f0cbfb26181ef3d220baa125354491
Mabalane <phumimabalane@gmail.com>
2024-08-30 16:37:42 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/Mabalane.-Github.io/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/Mabalane69.-Github.io/" : "/",
 };
